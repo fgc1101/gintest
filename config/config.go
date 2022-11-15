3950253ae6e425b550197e0d9874aaaab4541578
fgc <544942125@qq.com>
2022-11-15 14:33:58 +0800

    数据库配置到yaml文件中

--- config/config.go
+++ config/config.go
@@ -1,8 +1,19 @@
 package config
 
 type Config struct {
-	Server  Server  `yaml:"server" json:"app"`
-	QQEmail QQEmail `yaml:"qq_email" json:"qq_email"`
+	Server     Server     `yaml:"server" json:"app"`
+	DataSource DataSource `yaml:"data_source" json:"data_source"`
+	QQEmail    QQEmail    `yaml:"qq_email" json:"qq_email"`
+}
+
+type DataSource struct {
+	DriverName string `yaml:"driver_name" json:"driver_name"`
+	Host       string `yaml:"host" json:"host"`
+	Port       int    `yaml:"port" json:"port"`
+	Database   string `yaml:"database" json:"database"`
+	Username   string `yaml:"username" json:"username"`
+	Password   string `yaml:"password" json:"password"`
+	Charset    string `yaml:"charset" json:"charset"`
 }
 
 type Server struct {
